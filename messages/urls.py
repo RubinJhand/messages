7423061b6a7687bcba314ba963f0b2a9d34b1585
Rubin J <rubin@praxis.local>
2020-08-21 15:17:19 -0700

    Add dynamic post route with reference to database

--- messages/urls.py
+++ messages/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import home_view, post_detail_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('posts/<int:post_id>', post_detail_view),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Post
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+  return HttpResponse("<h1>Hello World</h1>")
+
+def post_detail_view(request, post_id, *args, **kwargs):
+  obj = Post.objects.get(id=post_id)
+  return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
