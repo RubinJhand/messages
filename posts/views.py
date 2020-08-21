e63e58956b467bb1e79661b6a064b342849f6b63
Rubin J <rubin@praxis.local>
2020-08-21 15:28:37 -0700

    Convert to REST API

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Post
 
@@ -8,5 +8,18 @@ def home_view(request, *args, **kwargs):
   return HttpResponse("<h1>Hello World</h1>")
 
 def post_detail_view(request, post_id, *args, **kwargs):
-  obj = Post.objects.get(id=post_id)
-  return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
+  
+  data = {
+    "id": post_id,
+    # "image_path": obj.image.url
+  }
+  status = 200
+  try:
+    obj = Post.objects.get(id=post_id)
+    data['content'] = obj.content
+  except:
+    data['message'] = "Not found"
+    status = 404
+
+
+  return JsonResponse(data, status=status)
