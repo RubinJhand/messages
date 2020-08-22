07ff7e1e0ed8356cae84f670e3f198489792f4bc
Rubin J <rubin@praxis.local>
2020-08-22 16:02:10 -0700

    Add user permissions

--- posts/views.py
+++ posts/views.py
@@ -15,10 +15,18 @@ def home_view(request, *args, **kwargs):
   return render(request, "pages/home.html", context={}, status=200)
 
 def post_create_view(request, *args, **kwargs):
+  user = request.user
+  if not request.user.is_authenticated:
+    user = None
+    if request.is_ajax():
+      return JsonResponse({}, status=401)
+    return redirect(settings.LOGIN_URL)
+
   form = PostForm(request.POST or None)
   next_url = request.POST.get("next") or None
   if form.is_valid():
     obj = form.save(commit=False)
+    obj.user = user
     obj.save()
     if request.is_ajax():
       return JsonResponse(obj.serialize(), status=201)
